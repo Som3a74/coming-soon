3034c921cf5a505c8fc9b3f04ca0ca770778a6b3
Som3a <som3a11202@gmail.com>
2025-04-16 00:00:47 +0200

    update date again

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
-// config.js
 export const CONFIG = {
     EMAILJS: {
         USER_ID: "pJyJsXSseo0VPcWdF",
         SERVICE_ID: "service_g056bpr",
         TEMPLATE_ID: "template_gu6w3ya"
     },
-    LAUNCH_DATE: new Date('2025-06-15T00:00:00'),
+    LAUNCH_DATE: new Date('2025-08-01T00:00:00'),
     LOCAL_STORAGE_KEY: 'kimcamLaunchDate',
     COUNTDOWN_REFRESH: 1000,
     EMAIL_REGEX: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
