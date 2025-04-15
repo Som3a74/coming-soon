1743d482137e326a7dda2cb782aed66bbd362746
Som3a <som3a11202@gmail.com>
2025-04-15 23:57:48 +0200

    update date

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-export const CONFIG={EMAILJS:{USER_ID:"pJyJsXSseo0VPcWdF",SERVICE_ID:"service_g056bpr",TEMPLATE_ID:"template_gu6w3ya"},LAUNCH_DATE:new Date('2025-06-15T00:00:00'),LOCAL_STORAGE_KEY:'kimcamLaunchDate',COUNTDOWN_REFRESH:1000,EMAIL_REGEX:/^[^\s@]+@[^\s@]+\.[^\s@]+$/};
+// config.js
+export const CONFIG = {
+    EMAILJS: {
+        USER_ID: "pJyJsXSseo0VPcWdF",
+        SERVICE_ID: "service_g056bpr",
+        TEMPLATE_ID: "template_gu6w3ya"
+    },
+    LAUNCH_DATE: new Date('2025-06-15T00:00:00'),
+    LOCAL_STORAGE_KEY: 'kimcamLaunchDate',
+    COUNTDOWN_REFRESH: 1000,
+    EMAIL_REGEX: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+};
